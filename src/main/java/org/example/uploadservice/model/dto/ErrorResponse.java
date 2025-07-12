4e21af994f5a76838372d26fa074838562614c87
NaGit09 <nhutanhtran806@gmail.com>
2025-07-13 00:27:51 +0700

    rename : rename error response file

--- src/main/java/org/example/uploadservice/model/dto/ErrorResponse.java
+++ src/main/java/org/example/uploadservice/model/dto/ErrorResponse.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ErrorRepsonse {
+public class ErrorResponse {
     private int code;
     private String message;
 }
--- src/main/java/org/example/uploadservice/utils/GenerateResponse.java
+++ src/main/java/org/example/uploadservice/utils/GenerateResponse.java
@@ -1,13 +1,13 @@
 package org.example.uploadservice.utils;
 
 import org.example.uploadservice.model.dto.APIResponse;
-import org.example.uploadservice.model.dto.ErrorRepsonse;
+import org.example.uploadservice.model.dto.ErrorResponse;
 import org.springframework.http.ResponseEntity;
 
 
 public class GenerateResponse {
     public static ResponseEntity<?> generateErrorResponse(Integer status, String message) {
-        return ResponseEntity.badRequest().body(new ErrorRepsonse(status, message));
+        return ResponseEntity.badRequest().body(new ErrorResponse(status, message));
     }
     public static ResponseEntity<?> generateSuccessResponse(Integer status ,String message , Object data) {
         return ResponseEntity.ok().body(new APIResponse<>(status, message, data)
